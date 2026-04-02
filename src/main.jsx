d13cd7f7f3572d93d72368b83fcce5db8e712942
tpk792023 <tpk792023@github.com>
2026-04-02 23:12:50 +0700

    Them file cau hinh CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // <--- DÒNG NÀY LÀ QUAN TRỌNG NHẤT ĐỂ CÓ GIAO DIỆN ĐẸP
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
