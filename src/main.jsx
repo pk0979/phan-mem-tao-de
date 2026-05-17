3b481afd00bce0b87f0c109530f79669bec20aa5
tpk792023 <tpk792023@github.com>
2026-05-17 15:34:00 +0700

    Cap nhat file

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- DÒNG NÀY LÀ QUAN TRỌNG NHẤT ĐỂ CÓ GIAO DIỆN ĐẸP
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
